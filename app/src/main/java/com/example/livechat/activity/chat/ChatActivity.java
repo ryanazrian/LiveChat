dd2d9e00f21796cc6f92affb69e6a026511d6f06
Ryan Azrian Syach <ryanazrian@pahamify.com>
2020-02-26 17:37:07 +0700

    adding findViewById on ChatActivity

--- app/src/main/java/com/example/livechat/activity/chat/ChatActivity.java
+++ app/src/main/java/com/example/livechat/activity/chat/ChatActivity.java
@@ -1,16 +1,27 @@
 package com.example.livechat.activity.chat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.livechat.R;
 
 public class ChatActivity extends AppCompatActivity {
 
+    private RecyclerView RC;
+    private EditText msg;
+    private Button sendBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        RC = findViewById(R.id.reyclerview_message_list);
+        msg = findViewById(R.id.edittext_chatbox);
+        sendBtn = findViewById(R.id.button_chatbox_send);
     }
 }
